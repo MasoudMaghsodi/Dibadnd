31e8b97195db74e993c4e57ac9d18d584d6456a6
reyhane fallah <reyhanefallah2002@gamil.com>
2023-09-21 01:04:34 +0330

    build route without binding

--- lib/view/saved_screen.dart
+++ lib/view/saved_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/component/widgets/appbar.dart';
-import 'package:flutter_application_1/view/home_screen.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:get/get.dart';
 
 class SavedScreen extends StatelessWidget {
   const SavedScreen({super.key});
