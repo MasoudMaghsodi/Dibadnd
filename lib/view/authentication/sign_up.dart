e7db90b202044cdcfb1ed05cf0bb800ca77ae986
MasoudMaghsodi <magsodi333@gmail.com>
2023-09-25 14:40:01 +0200

    update

--- lib/view/authentication/sign_up.dart
+++ lib/view/authentication/sign_up.dart
@@ -1,5 +1,5 @@
+import 'package:Dibadnd/controller/signup_controller.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/controller/signup_controller.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 
